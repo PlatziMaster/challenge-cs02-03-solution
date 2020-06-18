8330d69238331214a4e4162564d164fb1f6155c6
Pablo Trinidad <p@pablotrinidad.me>
2020-06-18 17:09:11 -0500

    Fixed linting problems.

--- 03/solution.py
+++ 03/solution.py
@@ -6,53 +6,81 @@ from typing import List
 
 class Solution:
     """Winner of an election."""
-    
-    def winner_brute_force(self, V):  # O(n^2) time, O(1) space
-        """Brute force solution."""
+
+    def winner_brute_force(self, V):  # pylint: disable=C0103
+        """Brute force solution.
+
+        Analysis:
+            - Time: O(n^2)
+            - Space: O(1)
+        """
         majority = len(V) // 2
-        for v in V:
+        for vote in V:
             count = 0
-            for w in V:
-                if w == v:
+            for vote_2 in V:
+                if vote_2 == vote:
                     count += 1
             if count > majority:
-                return v
+                return vote
+        return None
 
-    def winner_memo(self, V):  # O(n) time, O(n) space
-        """Use auxiliary storage."""
+    def winner_memo(self, V):  # pylint: disable=C0103
+        """Use auxiliary storage.
+
+        Analysis:
+            - Time: O(n)
+            - Space: O(n)
+        """
         store = defaultdict(int)
         majority = len(V) // 2
-        for v in V:
-            store[v] += 1
-        for k, v in store.items():
-            if v > majority:
+        for vote in V:
+            store[vote] += 1
+        for k, vote in store.items():
+            if vote > majority:
                 return k
-    
-    def winner_sort(self, V):  # O(nlogn) time, O(n) space
-        """Sorts the array, and find the winner."""
-        #return sorted(V)[len(V)//2]
-        sorted_votes = sorted(V)  # O(nlogn)
+        return None
+
+    def winner_sort(self, V):  # pylint: disable=C0103
+        """Sorts the array, and find the winner.
+
+        Analysis:
+            - Time: O(nlogn) time
+            - Space: O(n) if new array is created, O(1) if sorted in place.
+        """
+        sorted_votes = sorted(V)
         mid = len(V) // 2
         return sorted_votes[mid]
 
-    def winner_count(self, V, winner, start, end):  # O(n)
+    def winner_count(self, V, winner, start, end):  # pylint: disable=C0103
+        """Return the number of occurrences of winner in V[start:end+1].
+
+        Analysis:
+            - Time: O(n)
+            - Space: O(1)
+        """
         count = 0
         for i in range(start, end+1):
             count += 1 if V[i] == winner else 0
         return count
 
-    def winner_recursive(self, V, start, end):
+    def winner_recursive(self, V, start, end):  # pylint: disable=C0103
+        """Solves the problem using a divide and conquer strategy.
+
+        Analysis:
+            - Time: O(nlogn)
+            - Space: O(logn)
+        """
         if start == end:
             return V[start]
-        
+
         mid = int((end-start)/2 + start)
-        
+
         winner_left = self.winner_recursive(V, start, mid)
         winner_right = self.winner_recursive(V, mid+1, end)
 
         if winner_left == winner_right:
             return winner_left
-        
+
         left_count = self.winner_count(V, winner_left, start, mid)
         right_count = self.winner_count(V, winner_right, mid+1, end)
 
